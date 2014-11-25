3ae78a1a50f05c31abd8687bbbcd45e831ec206f
Mikhail Kolmogorov <fenderglass@gmail.com>
2014-11-24 18:29:19 -0800

    reduction 1 works sometimes

---
+++ minisat/simp/depend.mk
@@ -0,0 +1,34 @@
+/home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/simp/Main.o /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/simp/Main.or /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/simp/Main.od /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/simp/Main.op: \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/simp/Main.cc \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/utils/System.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/mtl/IntTypes.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/utils/ParseUtils.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/utils/Options.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/mtl/Vec.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/mtl/XAlloc.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/core/Dimacs.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/core/SolverTypes.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/mtl/Alg.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/mtl/Map.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/mtl/Alloc.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/simp/SimpSolver.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/mtl/Queue.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/core/Solver.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/mtl/Heap.h
+/home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/simp/SimpSolver.o /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/simp/SimpSolver.or /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/simp/SimpSolver.od /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/simp/SimpSolver.op: \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/simp/SimpSolver.cc \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/mtl/Sort.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/mtl/Vec.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/mtl/IntTypes.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/mtl/XAlloc.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/simp/SimpSolver.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/mtl/Queue.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/core/Solver.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/mtl/Heap.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/mtl/Alg.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/utils/Options.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/utils/ParseUtils.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/core/SolverTypes.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/mtl/Map.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/mtl/Alloc.h \
+ /home/fenderglass/Bioinf/UCSD/CSE200/sudoku2cnf/minisat/utils/System.h
--- sudoku2cnf.py
+++ sudoku2cnf.py
@@ -1,14 +1,32 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
 import numpy
 import random
-from sys import argv
+import sys
+from itertools import combinations, product
+from cStringIO import StringIO
+import subprocess
+
+
+MINISAT_EXEC = "minisat/minisat"
+TMP_INPUT = "sudoku_cnf.txt"
+TMP_OUTPUT = "minisat_out.txt"
 
 def pos2variable(size, row, column, val):
-    varPerRow = size*size
-    varPerCol = size
-    x = row * varPerRow
-    x += column * varPerCol
-    x += val
-    return x + 1
+    variable = val + (column * size) + (row * size * size)
+    return variable + 1
+
+
+def var2pos(variable, size):
+    variable = abs(variable) - 1
+    value = variable % size
+    variable = variable / size
+    column = variable % size
+    row = variable / size
+    return row, column, value
+
+
 
 def valid_row_values(sudoku, row, size):
     row = sudoku[row - 1, :]
@@ -16,12 +34,14 @@ def valid_row_values(sudoku, row, size):
     all_vals = numpy.arange(1, size + 1)
     return numpy.setdiff1d(all_vals, row_vals)
 
+
 def valid_col_values(sudoku, col, size):
     col = sudoku[:, col - 1]
     col_vals = col[col != 0]
     all_vals = numpy.arange(1, size + 1)
     return numpy.setdiff1d(all_vals, col_vals)
-    
+
+
 def valid_box_values(sudoku, row, col, size):
     box_size = int(size ** 0.5)
     box_row = ((row - 1) / box_size) * box_size
@@ -32,12 +52,14 @@ def valid_box_values(sudoku, row, col, size):
     all_vals = numpy.arange(1, size + 1)
     return numpy.setdiff1d(all_vals, box_vals)
 
+
 def valid_values(sudoku, row, col, size):
     row_vals = valid_row_values(sudoku, row, size)
     col_vals = valid_col_values(sudoku, col, size)
     box_vals = valid_box_values(sudoku, row, col, size)
     valid_vals = numpy.intersect1d(row_vals, col_vals)
     return numpy.intersect1d(valid_vals, box_vals)
+
 
 def random_sudoku(size):
     sudoku = numpy.zeros((size, size))
@@ -50,88 +72,101 @@ def random_sudoku(size):
         sudoku[i, i] = random.choice(d1)
     return sudoku
 
+
+def draw_result(sat_output, size):
+    result = None
+    with open(sat_output, "r") as f:
+        ans = f.readline().strip()
+        assert ans == "SAT"
+        result = map(int, f.readline().strip().split())
+        del result[-1]
+
+    sudoku = [[0 for _ in xrange(size)] for _ in xrange(size)]
+    for v in result:
+        if v < 0:
+            continue
+
+        row, col, value = var2pos(v, size)
+        assert sudoku[row][col] == 0
+        sudoku[row][col] = value + 1
+
+    for i in xrange(size):
+        for j in xrange(size):
+            print("%2d " % sudoku[i][j], end="")
+        print("")
+
+
 def reduction1(sudoku):
     size = len(sudoku)
     box_size = int(size ** 0.5)
     variables = size ** 3
-    temp_file = open('temp.txt', 'w')
+    buffer = StringIO()
     clauses = 0
 
+    ###
+    buffer.write('c initial conditions (clause type 3)\n')
     for row in xrange(size):
         for col in xrange(size):
-            val = sudoku[row, col]
+            val = int(sudoku[row, col])
             if val != 0:
-                temp_file.write("%d 0\n" % pos2variable(size, row, col, val))
-                for row2 in xrange(size):
-                    if row2 != row:
-                        variable = pos2variable(size, row2, col, val)
-                        temp_file.write("-%d 0\n" % variable)
-                        clauses += 1
-                for col2 in xrange(size):
-                    if col2 != col:
-                        variable = pos2variable(size, row, col2, val)
-                        temp_file.write("-%d 0\n" % variable)
-                        clauses += 1
-                box_row = (row / box_size) * box_size
-                box_col = (col / box_size) * box_size
-                for i in xrange(box_row, box_row + box_size):
-                    for j in xrange(box_col, box_col + box_size):
-                        if i != row and j != col:
-                            variable = pos2variable(size, i, j, val)
-                            temp_file.write("-%d 0\n" % variable)
-                            clauses += 1
+                buffer.write("%d 0\n" % pos2variable(size, row, col, val - 1))
+                clauses += 1
 
-
+    ###
+    buffer.write('c each cell has a certain number (clause type 1)\n')
     for row in xrange(size):
         for col in xrange(size):
             for val in xrange(size):
                 variable = pos2variable(size, row, col, val)
-                temp_file.write("%d" %variable)
-                if val != size - 1:
-                    temp_file.write(" ")
-                clauses += 1
-            temp_file.write(' 0\n')
-    
+                buffer.write('%d ' % variable)
+            buffer.write('0\n')
+            clauses += 1
+
+    ###
+    buffer.write('c sudoku rule for rows (clause type 2)\n')
     for row in xrange(size):
-        for col1 in xrange(size):
-            for col2 in xrange(col1 + 1, size):
-                for val in xrange(size):
-                    variable1 = pos2variable(size, row, col1, val)
-                    variable2 = pos2variable(size, row, col2, val)
-                    temp_file.write("-%d -%d 0\n" % (variable1, variable2))
-                    clauses += 1
-    
+        for val in xrange(size):
+            for col_1, col_2 in combinations(xrange(size), 2):
+                variable1 = pos2variable(size, row, col_1, val)
+                variable2 = pos2variable(size, row, col_2, val)
+                buffer.write("-%d -%d 0\n" % (variable1, variable2))
+                clauses += 1
+
+    ###
+    buffer.write('c sudoku rule for columns (clause type 2)\n')
     for col in xrange(size):
-        for row1 in xrange(size):
-            for row2 in xrange(row1 + 1, size):
-                for val in xrange(size):
-                    variable1 = pos2variable(size, row1, col, val)
-                    variable2 = pos2variable(size, row2, col, val)
-                    temp_file.write("-%d -%d 0\n" % (variable1, variable2))
+        for val in xrange(size):
+            for row_1, row_2 in combinations(xrange(size), 2):
+                variable1 = pos2variable(size, row_1, col, val)
+                variable2 = pos2variable(size, row_2, col, val)
+                buffer.write("-%d -%d 0\n" % (variable1, variable2))
+                clauses += 1
+
+    ###
+    buffer.write('c sudoku rule for boxes (clause type 2)\n')
+    for box_id in xrange(box_size):
+        box_row = (box_id % box_size) * box_size
+        box_col = (box_id / box_size) * box_size
+
+        for num in xrange(size):
+            for row_1, row_2 in product(xrange(box_row, box_row + box_size),
+                                        repeat=2):
+                for col_1, col_2 in product(xrange(box_col, box_col + box_size),
+                                            repeat=2):
+                    if row_1 == row_2 and col_1 == col_2:
+                        continue
+
+                    variable1 = pos2variable(size, row_1, col_1, num)
+                    variable2 = pos2variable(size, row_2, col_2, num)
+                    buffer.write("-%d -%d 0\n" % (variable1, variable2))
                     clauses += 1
 
-    for box in xrange(size):
-        box_row = (box / box_size) * box_size
-        box_col = (box % box_size) * box_size
-        for row1 in xrange(box_row, box_row + box_size):
-            for col1 in xrange(box_col, box_col + box_size):
-                for row2 in xrange(box_row, box_row + box_size):
-                    for col2 in xrange(box_col, box_col + box_size):
-                        if row1 != row2 and col1 != col2:
-                            for num in xrange(size):
-                                variable1 = pos2variable(size, row1, col1, num)
-                                variable2 = pos2variable(size, row2, col2, num)
-                                temp_file.write("-%d -%d 0\n" % (variable1, variable2))
-                                clauses += 1
-
-    cnf_file = open("sudoku_cnf.txt", "w")
-    cnf_file.write("p cnf %d %d\n" % (variables, clauses))
-    temp_file.close()
-    for line in open("temp.txt", 'r'):
-        cnf_file.write(line)
-    cnf_file.close()
+    with open(TMP_INPUT, "w") as cnf_file:
+        cnf_file.write("p cnf %d %d\n" % (variables, clauses))
+        cnf_file.write(buffer.getvalue())
 
 
+"""
 def reduction2(sudoku):
     size = len(sudoku)
     box_size = int(size ** 0.5)
@@ -213,9 +248,32 @@ def reduction2(sudoku):
     for line in open("temp.txt", 'r'):
         cnf_file.write(line)
     cnf_file.close()
+"""
 
-if __name__ == '__main__':
-    size = int(argv[1])
+
+def run_minisat(input_file, output_file):
+    cmd = [MINISAT_EXEC, input_file, output_file]
+    subprocess.call(cmd)
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage sudiku2cnf.py size", file=sys.stderr)
+        return 1
+
+    size = int(sys.argv[1])
+    if int(size ** 0.5) ** 2 != size:
+        print("Size should be a squared number", file=sys.stderr)
+        return 1
+
     sudoku = random_sudoku(size)
     reduction1(sudoku)
-    reduction2(sudoku)
+    run_minisat(TMP_INPUT, TMP_OUTPUT)
+    print("")
+    draw_result(TMP_OUTPUT, size)
+
+    #reduction2(sudoku)
+    return 0
+
+if __name__ == '__main__':
+    main()
