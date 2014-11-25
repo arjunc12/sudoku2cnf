eb77846424c1ffc705749875fbed1be46abcd1c0
Mikhail Kolmogorov <fenderglass@gmail.com>
2014-11-24 22:27:12 -0800

    second reduction working

--- sudoku2cnf.py
+++ sudoku2cnf.py
@@ -27,48 +27,44 @@ def var2pos(variable, size):
     return row, column, value
 
 
-
-def valid_row_values(sudoku, row, size):
-    row = sudoku[row - 1, :]
-    row_vals = row[row != 0]
-    all_vals = numpy.arange(1, size + 1)
-    return numpy.setdiff1d(all_vals, row_vals)
-
-
-def valid_col_values(sudoku, col, size):
-    col = sudoku[:, col - 1]
-    col_vals = col[col != 0]
-    all_vals = numpy.arange(1, size + 1)
-    return numpy.setdiff1d(all_vals, col_vals)
-
-
-def valid_box_values(sudoku, row, col, size):
-    box_size = int(size ** 0.5)
-    box_row = ((row - 1) / box_size) * box_size
-    box_col = ((col - 1) / box_size) * box_size
-    box = sudoku[box_row:box_row + box_size, box_col:box_col + box_size]
-    box = numpy.array(box).flatten()
-    box_vals = box[box != 0]
-    all_vals = numpy.arange(1, size + 1)
-    return numpy.setdiff1d(all_vals, box_vals)
-
-
-def valid_values(sudoku, row, col, size):
-    row_vals = valid_row_values(sudoku, row, size)
-    col_vals = valid_col_values(sudoku, col, size)
-    box_vals = valid_box_values(sudoku, row, col, size)
-    valid_vals = numpy.intersect1d(row_vals, col_vals)
-    return numpy.intersect1d(valid_vals, box_vals)
-
-
 def random_sudoku(size):
+    def valid_row_values(sudoku, row):
+        row = sudoku[row - 1, :]
+        row_vals = row[row != 0]
+        all_vals = numpy.arange(1, size + 1)
+        return numpy.setdiff1d(all_vals, row_vals)
+
+    def valid_col_values(sudoku, col):
+        col = sudoku[:, col - 1]
+        col_vals = col[col != 0]
+        all_vals = numpy.arange(1, size + 1)
+        return numpy.setdiff1d(all_vals, col_vals)
+
+    def valid_box_values(sudoku, row, col):
+        box_size = int(size ** 0.5)
+        box_row = ((row - 1) / box_size) * box_size
+        box_col = ((col - 1) / box_size) * box_size
+        box = sudoku[box_row:box_row + box_size, box_col:box_col + box_size]
+        box = numpy.array(box).flatten()
+        box_vals = box[box != 0]
+        all_vals = numpy.arange(1, size + 1)
+        return numpy.setdiff1d(all_vals, box_vals)
+
+    def valid_values(sudoku, row, col):
+        row_vals = valid_row_values(sudoku, row)
+        col_vals = valid_col_values(sudoku, col)
+        box_vals = valid_box_values(sudoku, row, col)
+        valid_vals = numpy.intersect1d(row_vals, col_vals)
+        return numpy.intersect1d(valid_vals, box_vals)
+
+    ###
     sudoku = numpy.zeros((size, size))
     for i in xrange(size):
-        r1 = valid_values(sudoku, 1, i + 1, size)
+        r1 = valid_values(sudoku, 1, i + 1)
         sudoku[0, i] = random.choice(r1)
-        c1 = valid_values(sudoku, i + 1, 1, size)
+        c1 = valid_values(sudoku, i + 1, 1)
         sudoku[i, 0] = random.choice(c1)
-        d1 = valid_values(sudoku, i + 1, i + 1, size)
+        d1 = valid_values(sudoku, i + 1, i + 1)
         sudoku[i, i] = random.choice(d1)
     return sudoku
 
@@ -144,11 +140,11 @@ def reduction1(sudoku):
 
     ###
     buffer.write('c sudoku rule for boxes (clause type 2)\n')
-    for box_id in xrange(box_size):
+    for box_id in xrange(size):
         box_row = (box_id % box_size) * box_size
         box_col = (box_id / box_size) * box_size
 
-        for num in xrange(size):
+        for val in xrange(size):
             for row_1, row_2 in product(xrange(box_row, box_row + box_size),
                                         repeat=2):
                 for col_1, col_2 in product(xrange(box_col, box_col + box_size),
@@ -156,9 +152,9 @@ def reduction1(sudoku):
                     if row_1 == row_2 and col_1 == col_2:
                         continue
 
-                    variable1 = pos2variable(size, row_1, col_1, num)
-                    variable2 = pos2variable(size, row_2, col_2, num)
-                    buffer.write("-%d -%d 0\n" % (variable1, variable2))
+                    variable_1 = pos2variable(size, row_1, col_1, val)
+                    variable_2 = pos2variable(size, row_2, col_2, val)
+                    buffer.write("-%d -%d 0\n" % (variable_1, variable_2))
                     clauses += 1
 
     with open(TMP_INPUT, "w") as cnf_file:
@@ -166,89 +162,70 @@ def reduction1(sudoku):
         cnf_file.write(buffer.getvalue())
 
 
-"""
 def reduction2(sudoku):
     size = len(sudoku)
     box_size = int(size ** 0.5)
     variables = size ** 3
-    temp_file = open('temp.txt', 'w')
+    buffer = StringIO()
     clauses = 0
 
+    ###
+    buffer.write('c initial conditions (clause type 3)\n')
     for row in xrange(size):
         for col in xrange(size):
-            val = sudoku[row, col]
+            val = int(sudoku[row, col])
             if val != 0:
-                temp_file.write("%d 0\n" % pos2variable(size, row, col, val))
-                for row2 in xrange(size):
-                    if row2 != row:
-                        variable = pos2variable(size, row2, col, val)
-                        temp_file.write("-%d 0\n" % variable)
-                        clauses += 1
-                for col2 in xrange(size):
-                    if col2 != col:
-                        variable = pos2variable(size, row, col2, val)
-                        temp_file.write("-%d 0\n" % variable)
-                        clauses += 1
-                box_row = (row / box_size) * box_size
-                box_col = (col / box_size) * box_size
-                for i in xrange(box_row, box_row + box_size):
-                    for j in xrange(box_col, box_col + box_size):
-                        if i != row and j != col:
-                            variable = pos2variable(size, i, j, val)
-                            temp_file.write("-%d 0\n" % variable)
-                            clauses += 1
+                buffer.write("%d 0\n" % pos2variable(size, row, col, val - 1))
+                clauses += 1
 
+    ###
+    buffer.write('c each cell has a certain number (clause type 1)\n')
     for row in xrange(size):
         for col in xrange(size):
-            for val in xrange(size):
-                variable = pos2variable(size, row, col, val)
-                temp_file.write("%d" %variable)
-                if val != size - 1:
-                    temp_file.write(" ") 
+            for val_1, val_2 in combinations(xrange(size), 2):
+                variable_1 = pos2variable(size, row, col, val_1)
+                variable_2 = pos2variable(size, row, col, val_2)
+                buffer.write('-%d -%s 0\n' % (variable_1, variable_2))
                 clauses += 1
-            temp_file.write(' 0\n')
-    
+
+    ###
+    buffer.write('c sudoku rule for rows (clause type 2)\n')
     for row in xrange(size):
         for val in xrange(size):
             for col in xrange(size):
-                variable = pos2variable(size, row, col, val)    
-                temp_file.write("%d" % variable)
-                if col != size - 1:
-                    temp_file.write(" ")
-                clauses += 1
-            temp_file.write(" 0\n")
+                variable = pos2variable(size, row, col, val)
+                buffer.write('%d ' % variable)
+            buffer.write('0\n')
+            clauses += 1
 
-    
+    ###
+    buffer.write('c sudoku rule for columns (clause type 2)\n')
     for col in xrange(size):
         for val in xrange(size):
             for row in xrange(size):
-                variable = pos2variable(size, row, col, val)    
-                temp_file.write("%d" % variable)
-                if row != size - 1:
-                    temp_file.write(" ")
-                clauses += 1
-            temp_file.write(" 0\n")
+                variable = pos2variable(size, row, col, val)
+                buffer.write('%d ' % variable)
+            buffer.write('0\n')
+            clauses += 1
 
-    for box in xrange(size):
-        box_row = (box / box_size) * box_size
-        box_col = (box % box_size) * box_size
+    ###
+    buffer.write('c sudoku rule for boxes (clause type 2)\n')
+    for box_id in xrange(size):
+        box_row = (box_id % box_size) * box_size
+        box_col = (box_id / box_size) * box_size
+
         for val in xrange(size):
             for row in xrange(box_row, box_row + box_size):
                 for col in xrange(box_col, box_col + box_size):
                     variable = pos2variable(size, row, col, val)
-                    temp_file.write("%d" % variable)
-                    if row != box_row + box_size - 1 and col != box_col + box_size - 1:
-                        temp_file.write(" ")
-                    clauses += 1
-            temp_file.write(" 0\n")
+                    buffer.write('%d ' % variable)
 
-    cnf_file = open("sudoku_cnf2.txt", "w")
-    cnf_file.write("p cnf %d %d\n" % (variables, clauses))
-    temp_file.close()
-    for line in open("temp.txt", 'r'):
-        cnf_file.write(line)
-    cnf_file.close()
-"""
+            buffer.write('0\n')
+            clauses += 1
+
+    with open(TMP_INPUT, "w") as cnf_file:
+        cnf_file.write("p cnf %d %d\n" % (variables, clauses))
+        cnf_file.write(buffer.getvalue())
 
 
 def run_minisat(input_file, output_file):
@@ -257,22 +234,28 @@ def run_minisat(input_file, output_file):
 
 
 def main():
-    if len(sys.argv) != 2:
-        print("Usage sudiku2cnf.py size", file=sys.stderr)
+    if len(sys.argv) != 3:
+        print("Usage sudoku2cnf.py [1|2] size", file=sys.stderr)
         return 1
 
-    size = int(sys.argv[1])
+    red_type = sys.argv[1]
+    size = int(sys.argv[2])
     if int(size ** 0.5) ** 2 != size:
         print("Size should be a squared number", file=sys.stderr)
         return 1
+    if red_type not in ["1", "2"]:
+        print("Reduction type could be either 1 or 2", file=sys.stderr)
+        return 1
 
     sudoku = random_sudoku(size)
-    reduction1(sudoku)
+    if red_type == "1":
+        reduction1(sudoku)
+    else:
+        reduction2(sudoku)
+
     run_minisat(TMP_INPUT, TMP_OUTPUT)
-    print("")
     draw_result(TMP_OUTPUT, size)
 
-    #reduction2(sudoku)
     return 0
 
 if __name__ == '__main__':
